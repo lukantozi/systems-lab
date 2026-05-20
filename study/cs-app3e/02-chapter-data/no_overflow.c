28051a2ae5af3138f24738f5c9e23b7cc2fe64bc
lukantozi <lukamamrik@proton.me>
2026-05-20 14:45:34 +0200

    csapp3e: two's complement no overflow solution

--- study/cs-app3e/02-chapter-data/no_overflow.c
+++ study/cs-app3e/02-chapter-data/no_overflow.c
@@ -10,8 +10,25 @@ int uadd_ok(unsigned x, unsigned y) {
      */
 }
 
+int tadd_k(int x, int y) {
+    if (x < 0 && y < 0 && x + y >= 0) return 0;
+    if (x >= 0 && y >= 0 && x + y < 0) return 0;
+    return 1;
+    /* book solution
+     * int sum = x+y;
+     * int neg_over = x < 0 && y < 0 && sum >= 0;
+     * int pos_over = x >= 0 && y >= 0 && sum < 0;
+     * return !neg_over && !pos_over;
+    */
+}
+
 int main(void) {
-    int ok = uadd_ok(4294967273, 23);
-    printf("%d\n", ok);
+    int uok = uadd_ok(4294967273, 23);
+    printf("%d\n", uok);
+
+    int tok = tadd_k(4294967273/3, 123);
+    printf("%d\n", tok);
+    int tok1 = tadd_k(4294967273/2, 123);
+    printf("%d\n", tok1);
     return 0;
 }
