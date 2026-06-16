4610eb5a2b789da5d1f7051c578ccf1d2b4c687a
lukantozi <lukamamrik@proton.me>
2026-06-16 20:10:58 +0200

    feat(mhex): add canonical output mode

--- tools/mhex/mhex.c
+++ tools/mhex/mhex.c
@@ -1,55 +1,73 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void hdump(FILE *f, int canon) {
     int c;
-    int col = 0;
+    int bytes = 0;
     int word2 = 0;
     int offset = 0;
+    int len = 0;
+    int size = canon? 80: 49;
+    char line[size];
     char buf[17];
-    printf("%.7x ", offset);
+
+    len += sprintf(line + len, "%.8x%s", offset, canon? "  ": " ");
     while ((c = fgetc(f)) != EOF) {
         if (!canon && word2 == 2) {
-            putchar(0x20);
+            line[len++] = 0x20;
             word2 = 0;
         }
-        // TODO: maybe move this logic after
-        if (col == 16) {
-            buf[col] = '\0';
-            if (canon) printf("|%s|\n", buf);
-            else putchar(0x0A);
-            printf("%.7x ", offset);
-            col = 0;
+
+        if (canon && bytes != 0 && (bytes % 8) == 0) {
+            line[len++] = 0x20;
         }
-        printf("%.2x%s", c, canon? " ": "");
-        if (canon && col != 0 && (col % 8) == 0) putchar(0x20);
-        // TODO: deal with non-printables
-        if (c != 0x0A) buf[col] = c;
-        col++;
+
+        len += sprintf(line + len, "%.2x%s", c, canon? " ": "");
+        buf[bytes] = (c > 31 && c < 127)? c: '.';
+        bytes++;
         word2++;
         offset++;
+
+        if (bytes == 16) {
+            buf[bytes] = '\0';
+            if (canon) len += sprintf(line + len, " |%s|%c", buf, '\n');
+            else line[len++] = 0x0A;
+
+            line[len] = '\0';
+            printf("%s", line);
+            len = 0;
+            len += sprintf(line + len, "%.8x%s", offset, canon? "  ": " ");
+            bytes = 0;
+            word2 = 0;
+            buf[0] = 0;
+        }
     }
+
+    buf[bytes] = '\0';
     if (canon) {
-        // TODO: make padding work
-        /*
-        int pad = 28 - col;
-        printf("%*c", pad, ' ');
-        */
-        buf[col] = '\0';
-        printf("|%s|\n", buf);
-        printf("%.7x\n", offset);
-    } else {
-        printf("\n%.7x\n", offset);
+        printf("%-*s|%s|\n%.8x\n", size-20, line, buf, offset);
     }
+    else printf("%s\n%.8x\n", line, offset);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        hdump(stdin, 1);
+    int opt;
+    int canon = 0;
+    if ((opt = getopt(argc, argv, "C")) != -1) {
+        switch (opt) {
+            case 'C':
+                canon = 1;
+                break;
+        }
+    }
+    if (argc == 1 + canon) {
+        int opt;
+        hdump(stdin, canon);
     } else {
-        for (int i = 1; i < argc; i++) {
+        for (int i = 1 + canon; i < argc; i++) {
             FILE *f = fopen(argv[i], "r");
             if (f == NULL) continue;
-            hdump(f, 1);
+            hdump(f, canon);
             fclose(f);
         }
     }
