672d6c88a7ef4c7089c6844d306dcf57ea28260c
lukantozi <lukamamrik@proton.me>
2026-07-22 14:45:33 +0200

    chore(vector): clean up vector testing

--- data-structures/tests/test_vector.c
+++ data-structures/tests/test_vector.c
@@ -3,6 +3,7 @@
 #include "assert.h"
 
 int main(void) {
+    /* test vec: append, get_size_inmem/inuse show_array*/
     vector *vec = init_vector(5);
     int status;
     for (size_t i = 0; i < 10; i++) {
@@ -10,44 +11,35 @@ int main(void) {
         assert(status == 1);
     }
 
-    printf("\nsize of vec: %zu\n", get_size_inmem(vec));
-    printf("elements in use in vec: %zu\n", get_size_inuse(vec));
+    assert(11 == get_size_inmem(vec));
+    assert(10 == get_size_inuse(vec));
     show_array(vec);
 
-    puts("\nappend 10");
     append_element(vec, 10);
-    printf("size of vec: %zu\n", get_size_inmem(vec));
-    printf("elements in use in vec: %zu\n", get_size_inuse(vec));
+    assert(11 == get_size_inmem(vec));
+    assert(11 == get_size_inuse(vec));
     show_array(vec);
 
-    puts("\nappend 11");
     append_element(vec, 11);
-    printf("size of vec: %zu\n", get_size_inmem(vec));
-    printf("elements in use in vec: %zu\n\n", get_size_inuse(vec));
+    assert(23 == get_size_inmem(vec));
+    assert(12 == get_size_inuse(vec));
     show_array(vec);
 
-    // assert(get_element(vec, 12) == 1);
-
-
-#if 1
-    size_t len = get_size_inuse(vec);
-    for (size_t i = 0; i < len; i++) {
-        printf("at index %zu: %d\n", i, get_element(vec, i));
-    }
-    show_array(vec);
-
+    /* test vec1: all above + get_element */
     vector *vec1 = init_vector(10);
     append_element(vec1, 5);
     append_element(vec1, 10);
     append_element(vec1, 15);
+    assert(10 == get_size_inmem(vec1));
+    assert(3 == get_size_inuse(vec1));
+    show_array(vec1);
     int sum = 0;
     for (int i = 0; i < 3; i++) {
         sum += get_element(vec1, i);
     }
-    show_array(vec1);
-    printf("%d\n", sum);
-    free_vector(vec1);
-#endif
+    assert(sum == 30);
 
+    /* free vecs */
     free_vector(vec);
+    free_vector(vec1);
 }
